86489eb27d6cedbcc26185a3fc6683547e5ae746
ShawSail.Xiao <ShawSail.Xiao@analyticservice.net>
2019-11-21 14:54:12 +0800

    Completed With Base Constant

---
+++ src/main/java/com/example/workflowcli/common/core/base/Constant.java
@@ -0,0 +1,14 @@
+package com.example.workflowcli.common.core.base;
+
+/**
+ * @author: AAS
+ * @create: 周四 11月 2019
+ * @description:
+ */
+public class Constant {
+    //restful
+    public static final String REQUEST_METHODS = "POST, PUT, GET, OPTIONS, DELETE";
+
+    //request header
+    public static final String REQUEST_HEADERS = "Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With,userId,token,Access-Control-Allow-Headers , token, sid";
+}
