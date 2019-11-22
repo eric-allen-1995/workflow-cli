638067f9c9c62cd6c0ba152a7444caee936ef07f
ShawSail.Xiao <ShawSail.Xiao@analyticservice.net>
2019-11-22 16:57:22 +0800

    exclude security

--- src/main/java/com/example/workflowcli/WorkflowCliApplication.java
+++ src/main/java/com/example/workflowcli/WorkflowCliApplication.java
@@ -2,10 +2,11 @@ package com.example.workflowcli;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 @MapperScan("com.example.workflowcli.*.mapper")
 public class WorkflowCliApplication {
 
