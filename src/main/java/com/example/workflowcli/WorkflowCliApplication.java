e174ca1397d0138db6cd651b630ae09da99c1bec
18AF8327DF8324D96098BEC3EB518D29 <ShawSail.Xiao@analyticservice.net>
2019-12-06 16:58:23 +0800

    fix: missing database configuration

--- src/main/java/com/example/workflowcli/WorkflowCliApplication.java
+++ src/main/java/com/example/workflowcli/WorkflowCliApplication.java
@@ -2,6 +2,7 @@ package com.example.workflowcli;
 
 import com.example.workflowcli.workflow.config.AppDispatcherServletConfiguration;
 import com.example.workflowcli.workflow.config.ApplicationConfiguration;
+import org.flowable.ui.modeler.conf.DatabaseConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
@@ -10,6 +11,8 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.context.annotation.Import;
 
 @Import(value = {
+        //因为引入conf,需自行引入DatabaseConfiguration,否则将导致500异常
+        DatabaseConfiguration.class,
         //引入修改后的配置
         ApplicationConfiguration.class,
         AppDispatcherServletConfiguration.class
